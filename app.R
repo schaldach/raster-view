e163e74500d3bea9073e7b4d28b3c860e94358cd
schaldach <gabimorgado0311@gmail.com>
2025-01-18 21:35:40 -0300

    Primeiro exemplo do terra com input built-in, salvando o arquivo inteiro. Acredito que será mais fácil modificar este componente do que criar um novo, mas posso estar errado

--- app.R
+++ app.R
@@ -4,55 +4,44 @@ if(!require(pacman)) {
 library(pacman)
 p_load(shiny, bslib, ggplot2, terra)
 
-# Get the data
-
-file <- "https://github.com/rstudio-education/shiny-course/raw/main/movies.RData"
-destfile <- "movies.RData"
-
-download.file(file, destfile)
-
-# Load data
-
-load("movies.RData")
-
-
 fileInputCustom <- function(inputId, label) {
   tags$div(
     class = "form-group shiny-input-container",
     tags$label(label, `for` = inputId),
-    tags$input(id = inputId, type = "file", class = "form-control")
+    tags$input(
+      id = inputId,
+      type = "file",
+      class = "form-control custom-file-input", # Add class for custom binding
+      onchange = sprintf("Shiny.onInputChange('%s', null); Shiny.sendCustomMessage('uploadFile', {inputId: '%s'})", inputId, inputId)
+    )
   )
 }
 
-
 # Define UI
-
 ui <- fluidPage(
-  fileInput("upload", "Upload a file"),
-  fileInputCustom("myFileInput", "Select a File: "),
-  tableOutput("files"),
-  tableOutput("files2")
+  tags$head(
+    tags$script(src = "customFileInput.js") # Include the JavaScript file
+  ),
+  fileInputCustom("upload", "Select a File: "),
+  imageOutput("files")
 )
 
 # Define server
-
 server <- function(input, output, session) {
-  output$files <- renderTable(input$upload)
-  
-  rasterData <- reactive({
-    req(input$myFileInput) # Ensure a file is uploaded before proceeding
-    
-    # Read the raster file using terra::rast
-    print(input$myFileInput)
-    #filePath <- input$myFileInput$datapath
-    #terra::rast(filePath)
+  observe({
+    req(input$upload)
+    print(input$upload)
   })
   
-  #terra_raster <- terra::rast(input$myFileInput$)
-  #output$files2 <- terra::plot(terra_raster)
+  output$files <- renderPlot({
+    req(input$upload)
+    terra_raster <- terra::rast(input$upload$datapath)
+    terra_raster[terra_raster > 10000] <- NA
+    terra::plot(terra_raster)
+  })
 }
 
-# Create a Shiny app object
-options(shiny.maxRequestSize = 500 * 1024^2)
+# options(shiny.maxRequestSize = 500 * 1024^2)
 
+# Create a Shiny app object
 shinyApp(ui = ui, server = server)

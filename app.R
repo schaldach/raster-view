7adf8d7ba45c1ef6c6481963c55f30b51793e4d7
schaldach <gabimorgado0311@gmail.com>
2025-01-26 13:36:06 -0300

    Tabela interativa e nome da layer, dinâmico. TODO: mudar layer mostrada com o clique na tabela, renderizar um texto placeholder da layer atual antes e todos os componentes que dependem do arquivo dentro de um único observe (para depois fazer a partição do arquivo, a ideia principal)

--- app.R
+++ app.R
@@ -2,7 +2,7 @@ if(!require(pacman)) {
     install.packages("pacman", dependencies = TRUE);
 }
 library(pacman)
-p_load(shiny, bslib, ggplot2, terra, leaflet, viridis)
+p_load(shiny, bslib, ggplot2, terra, leaflet, viridis, DT)
 
 fileInputCustom <- function(inputId, label, value = 0) {
   tags$div(
@@ -11,6 +11,8 @@ fileInputCustom <- function(inputId, label, value = 0) {
     tags$input(id = inputId, type = "file", class = "form-control", value = value)
   )
 }
+
+activeLayerIndex <- 1
 
 # Define UI
 ui <- page_sidebar(
@@ -31,10 +33,11 @@ ui <- page_sidebar(
   ),
   sidebar = sidebar(
     fileInput("upload", "Select a File: "),
-    tableOutput("names"),
+    DTOutput("names"),
     width = 325,
     padding = 8
   ),
+  htmlOutput("text"),
   leafletOutput("rastermap")
 )
 
@@ -45,9 +48,23 @@ server <- function(input, output, session) {
     print(input$upload)
   })
   
+  # div para não sofrer a estilização "flex column" e HTML para renderizar o texto em negrito
+  output$text <- renderUI({ 
+    HTML("Select a NetCDF file to visualize a layer")
+  })
+# pelo visto nao da pra ter 2 renders no mesmo output. como seria feito? depois farei tudo dentro de um observe imagino,
+  # sera que funcionaria?
+  output$text <- renderUI({ 
+    req(input$upload)
+    terra_raster <- terra::rast(input$upload$datapath)
+    layer_name <- names(terra_raster)[[activeLayerIndex]]
+    
+    tags$div(HTML(paste0("Plotting layer <b>", layer_name, "</b>")))
+  })
+  
   output$rastermap <- renderLeaflet({
     req(input$upload)
-    terra_raster <- terra::rast(input$upload$datapath)[[1]] # apenas primeira camada
+    terra_raster <- terra::rast(input$upload$datapath)[[activeLayerIndex]] # apenas primeira camada
     terra_raster[terra_raster > 10000] <- NA
 
     pal <- colorNumeric(palette = "viridis", domain = values(terra_raster), na.color = NA)
@@ -60,7 +77,7 @@ server <- function(input, output, session) {
       # setMaxBounds(-180, -90, 180, 90)
   })
   
-  output$names <- renderTable({
+  output$names <- renderDT({
     req(input$upload)
     terra_raster <- terra::rast(input$upload$datapath)
     
@@ -70,7 +87,12 @@ server <- function(input, output, session) {
 
     raster_info <- cbind(names(terra_raster), formatted_time, varnames(terra_raster), units(terra_raster))
     colnames(raster_info) <- c("Layer", "Time", "Variable", "Unit")
-    raster_info
+
+    datatable(
+      raster_info,
+      selection = list(mode="single", selected = 1),
+      options = list(dom = 't')
+    )
   })
 }
 
